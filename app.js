5ae89f9374a71945fb330482bc81142b0e91c8cb
lisadelladucata <lisa.delladucata@gmail.com>
2025-01-21 00:45:25 +0100

    feat:add-first-step

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
-console.log('test')
+const express = require('express')
+const app = express()
+const port = 3001
+
+app.get('/', (req, res) => {
+  res.send('Server del mio blog')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
